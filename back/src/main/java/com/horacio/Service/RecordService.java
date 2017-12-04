6a5b754418ff194860546539c899cb2e85e19e3b
杜盛瑀 <772773671@qq.com>
2017-12-04 22:19:57 +0800

    修改Bug,归还设备接口

--- back/src/main/java/com/horacio/Service/RecordService.java
+++ back/src/main/java/com/horacio/Service/RecordService.java
@@ -95,12 +95,15 @@ public class RecordService {
 
     public Boolean returnItem(JsonNode data,String userid) throws Exception{
         Record record =recordRepository.findOne(data.get("id").intValue());
+        if(record == null){
+            throw new LabsException(ResultEnum.OBJECT_NOT_FOUND.getCode(),ResultEnum.OBJECT_NOT_FOUND.getMsg());
+        }
         Facility item =facilityRepository.findOneByItemName(record.getItemName());
         if(item == null){
             throw new LabsException(ResultEnum.OBJECT_NOT_FOUND.getCode(),ResultEnum.OBJECT_NOT_FOUND.getMsg());
         }
         Admin user = adminRepository.findOne(Integer.valueOf(userid));
-        record.setReturnTime(new Date(Long.valueOf(data.get("returnTime").intValue())));
+        record.setReturnTime(new Date(Long.valueOf(data.get("returnTime").textValue())));
         record.setReturnOperator(user.getName());
         recordRepository.save(record);
         int remianNum = item.getRemainNum();
